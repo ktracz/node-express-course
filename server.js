a8b4b72d113d599f4ab81724757e5e79f22071d3
Kamil Tracz <kamil.kacper.tracz@gmail.com>
2021-05-30 00:30:01 -0400

    add first GET route

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const app = express();
 
-
-
-app.listen(8000,function(){
-  console.log("server is running")
-})
-
-const express = require('express');
-const app = express();
-
 const mockUserData=[
   {name: 'Mark'},
   {name: 'Jill'}
